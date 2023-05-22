6186338c99d774c1332e3d4bedaac8525b81003c
dPwls <kimphoby0125@gmail.com>
2023-05-22 12:08:26 +0900

    Docs : 과제제출

--- java02/src/ex02/IntWithInteger.java
+++ java02/src/ex02/IntWithInteger.java
@@ -1,38 +1,38 @@
 package ex02;
 class MyInteger{
-    private int n;
+    private int n ;
     public MyInteger(int n) {
         this.n = n;
     }
     public int getN() {
         return n;
     }
-
     public void setN(int n) {
         this.n = n;
     }
 }
 public class IntWithInteger {
+    public static void intMul42(int n){
+        n *= 42;
+    };
+    public static void integerTo42(Integer integer){
+        integer = 42;
+    };
+    public static void myIntegerDiv42(MyInteger myInteger){
+        myInteger.setN(myInteger.getN() / 42) ;
+    };
 
-    public static void varInt(int num){
-        num *= 42 ;
-    }
-    public static void varInteger(Integer num){
-        num = 42 ;
-    }
-    public static void div42(MyInteger num){
-        int n = num.getN();
-        num.setN(n / 42);
-    }
     public static void main(String[] args) {
-        Integer num2 = new Integer(20);
-        MyInteger myInteger = new MyInteger(420);
-         int num1 = 10;
-        varInt(num1);
-        System.out.println(num1);
-        varInteger(num2);
-        System.out.println(num2);
-        div42(myInteger);
-        System.out.println(myInteger.getN());
+        int num = 1; // primitive
+        Integer integer = new Integer(1); // wrapper(객체), but 불변
+        MyInteger myInteger = new MyInteger(42*42); // 객체, 모든 객체는 주소 값을 가짐. 포인터처럼 사용 가능.
+        intMul42(num);
+        integerTo42(integer);
+        myIntegerDiv42(myInteger);
+        System.out.println("int 변수를 매개변수로 받아서 처리 : " + num );
+        System.out.println("Intger 변수를 매개변수로 받아서 처리 :" + integer);
+        System.out.println("MyInteger 변수를 매개변수로 받아서 처리: " + myInteger.getN());
     }
+
+
 }
